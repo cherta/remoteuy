70e286711001840b53a90d28896a88d6fd65a77b
Gabriel Chertok <gchertok@gmail.com>
2020-03-13 13:06:54 -0300

    Support urls for companies
    - Add url on `data/companies.js`
    - Support the url and non-url version on `Company` component

--- data/companies.js
+++ data/companies.js
@@ -1,6 +1,7 @@
 export default [
   {
     picture: "https://www.ingenious.agency/favicon-32x32.png",
+    url: "https://www.ingenious.agency",
     name: "Ingenious",
     description: "Fueling the next healthcare industry.",
     meta: {
