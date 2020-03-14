98155dfbf3c4245d1a823c0df07e004acd724814
TheAntiAfro <loming68@gmail.com>
2020-03-14 19:13:15 -0300

    Update companies.js

--- data/companies.js
+++ data/companies.js
@@ -97,6 +97,15 @@ export default [
     meta: {
       allowFullRemote: true,
       hasPhysicalOffices: true
+    },
+  {
+    picture: "https://secureservercdn.net/166.62.115.254/f46.324.myftpupload.com/wp-content/uploads/2018/02/favicon.png",
+    url: "https://www.inswitch.com/",
+    name: "IN Switch",
+    description: "Leading mobile financial innovation",
+    meta: {
+      allowFullRemote: true,
+      hasPhysicalOffices: true
     }
   }
 ];
