4caa82ca8d7b4fa1b8a58f88c9711deb6e519e1f
Nicolás Corso <ncorso@gmail.com>
2020-03-16 12:42:43 -0300

    Update companies.js

--- data/companies.js
+++ data/companies.js
@@ -229,5 +229,15 @@ export default [
       allowFullRemote: true,
       hasPhysicalOffices: false
     }
+  },
+  {
+    picture: "https://www.bloque.com.uy/out-images/bloque-logo.svg",
+    url: "https://www.bloque.com.uy/",
+    name: "Bloque",
+    description: "Somos un estudio de diseño y desarrollo web enfocado en crear hermosas interfaces. • Obsesionados por los detalles, performance, y SEO •",
+    meta: {
+      allowFullRemote: true,
+      hasPhysicalOffices: true
+    }
   }
 ];
