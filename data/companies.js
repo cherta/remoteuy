e238cc6f6903925bba9cbabea609b108c60187d3
Javier Lempert <javier@lightit.io>
2020-03-15 23:48:19 -0300

    Add Light-it

--- data/companies.js
+++ data/companies.js
@@ -128,5 +128,15 @@ export default [
       allowFullRemote: true,
       hasPhysicalOffices: false
     }
+  },
+  {
+    picture: "https://lightit.io/favicon.ico",
+    url: "https://lightit.io/",
+    name: "Light-it",
+    description: "We are a digital product studio that combines strategic thinking and the latest technologies to craft innovative solutions that consistently break ground.",
+    meta: {
+      allowFullRemote: true,
+      hasPhysicalOffices: true
+    }
   }
 ];
