2789064f5a04170520fb5200a58462ab57b3dee5
Guillermo Rodríguez <guillermo85@gmail.com>
2020-03-16 12:33:49 -0300

    Added Datasec Company (#33)
    * Added Datasec Company
    
    * Added Datasec Company
    
    Added Datasec Company
    
    Co-authored-by: Gabriel Chertok <gchertok@gmail.com>

--- data/companies.js
+++ data/companies.js
@@ -209,5 +209,15 @@ export default [
       allowFullRemote: false,
       hasPhysicalOffices: true
     }
+  },
+  {
+    picture: "https://datasec-soft.com/favicon.ico",
+    url: "https://datasec-soft.com/",
+    name: "Datasec",
+    description: "Somos una firma presente en el mercado internacional desde 1987, pionera en el área del buen gobierno, aseguramiento, gestión de riesgos, calidad y seguridad en sistemas de la información (TI).",
+    meta: {
+      allowFullRemote: true,
+      hasPhysicalOffices: true
+    }
   }
 ];
