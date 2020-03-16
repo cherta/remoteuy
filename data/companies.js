7878801179a1592d6e40e144a4be2b8d683cdb61
Guillermo Fernandez <ollita7@gmail.com>
2020-03-16 12:55:14 -0300

    Cavepot add physical offices (#40)
    * add cavepot company
    
    * add physical offices
    
    Co-authored-by: Gabriel Chertok <gchertok@gmail.com>

--- data/companies.js
+++ data/companies.js
@@ -227,7 +227,7 @@ export default [
     description: "We are a software and app design company keen on pixel perfect solutions that bring your ideas to live!",
     meta: {
       allowFullRemote: true,
-      hasPhysicalOffices: false
+      hasPhysicalOffices: true
     }
   },
   {
