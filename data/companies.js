f85344b1a6e9c12a726e68d7aba627eb79df9342
cesar <cesar.reyes@toolfactory.net>
2020-03-18 14:38:49 -0300

    fix bluekiri logo

--- data/companies.js
+++ data/companies.js
@@ -201,7 +201,7 @@ export default [
     }
   },
   {
-    picture: "https://cloud.bluekiri.com/images/bluekiri.svg",
+    picture: "https://bluekiri.com/images/bluekiri_leaf.png",
     url: "https://bluekiri.com/",
     name: "Bluekiri",
     description: "Bluekiri has the team, the knowledge and the technology to architect and manage your digital business. We provide you with a high performance, high availability, cost effective global solution.",
