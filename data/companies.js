01d55f7b73cc939961f269c1a47bad680132b68a
Rafael Sisto <rsisto@kreitech.io>
2020-05-13 18:30:45 -0300

    updating kreitech logo

--- data/companies.js
+++ data/companies.js
@@ -1,6 +1,6 @@
 export default [
  {
-    picture: "https://scontent.fmvd1-1.fna.fbcdn.net/v/t1.0-9/12009649_1680564098845933_8958272112656141499_n.png?_nc_cat=108&_nc_sid=85a577&_nc_ohc=6F5ZhpxyDc0AX8ZMnb5&_nc_ht=scontent.fmvd1-1.fna&oh=528c41bc8aa02f2306fec22942ffd98c&oe=5E9354CF",
+    picture: "https://www.cuti.org.uy/data/f/c/socios/660/160x160/logo-kreithech.png",
     url: "https://kreitech.io",
     name: "Kreitech",
     description: "Enabling ideas through software and Hardware. Software and IoT Agency",
