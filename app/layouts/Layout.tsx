e48604253c496dba4316443e63c4c58502df9889
Gabriel Chertok <gchertok@gmail.com>
2020-09-19 08:40:00 -0300

    Fix TS issue

--- app/layouts/Layout.tsx
+++ app/layouts/Layout.tsx
@@ -105,7 +105,7 @@ const Header = () => {
                       {company.name}
                     </Link>
                   ))
-                  .reduce((prev, curr) => [prev, ", ", curr])}
+                  .reduce((prev, curr) => [prev, ", ", curr] as any)}
               </MenuItem>
             </>
           )}
